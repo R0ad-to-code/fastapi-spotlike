3a792fd7947908a54411f4f56b9af1a1d7ce2c91
Max Mengeringhausen <max.mengeringhausen.jean@gmail.com>
2025-06-17 17:45:01 +0200

    ajout defaults var terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,21 @@
 variable "project_name" {
   description = "Nom du projet"
   type        = string
+  default = "Spotilike"
+
 }
 
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
+  default = "eu-west-3"
+
 }
 
 variable "environment" {
   description = "The deployment environment"
   type        = string
+  default = "prod"
 }
 
 variable "db_password" {
